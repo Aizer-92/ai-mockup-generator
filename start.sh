f261384c2242ca4b846c42661939a10dc426eefb
Aizer-92 <aizer1992@gmail.com>
2025-09-08 15:20:28 +0300

    Fix Streamlit port configuration using environment variables

--- start.sh
+++ start.sh
@@ -3,5 +3,9 @@
 # Get port from Railway environment variable, default to 8501
 PORT=${PORT:-8501}
 
+# Set Streamlit environment variables
+export STREAMLIT_SERVER_PORT=$PORT
+export STREAMLIT_SERVER_ADDRESS=0.0.0.0
+
 # Start Streamlit
-streamlit run main.py --server.port=$PORT --server.address=0.0.0.0
+streamlit run main.py
